205644857c76891c989853dd8bbde12466bee308
Christopher Nethercott <ccnethercott@gmail.com>
2019-02-19 21:27:49 +0000

    Added config.yml and matching code

--- src/main/java/com/arwenmc/Primis.java
+++ src/main/java/com/arwenmc/Primis.java
@@ -1,23 +1,29 @@
 package com.arwenmc;
 
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Primis extends JavaPlugin {
 
-    PluginDescriptionFile config = this.getDescription();
+    PluginDescriptionFile plugin = this.getDescription();
+    FileConfiguration config = this.getConfig();
 
-    public final String NOT_PLAYER = ChatColor.RED + "You must be a player to use this command.";
-    public final String NO_PERMISSION = ChatColor.RED + "You do not have permission to use this command.";
+    public final String NOT_PLAYER = (config.getString("messages.not_player") == null) ? ChatColor.RED + "You must be a player to use this command." : config.getString("messages.not_player");
+    public final String NO_PERMISSION = (config.getString("messages.no_permission") == null) ? ChatColor.RED + "You do not have permission to use this comnmand." : config.getString("messages.no_permission");
 
     @Override
     public void onEnable() {
-        getLogger().info(config.getFullName() + " version: " + config.getVersion() + " has been enabled.");
+        getLogger().info(plugin.getFullName() + " version: " + plugin.getVersion() + " has been enabled.");
+
+        config.options().copyDefaults(true);
+        saveConfig();
     }
 
     @Override
     public void onDisable() {
-        getLogger().info(config.getFullName() + " version: " + config.getVersion() + " has been disabled.");
+        getLogger().info(plugin.getFullName() + " version: " + plugin.getVersion() + " has been disabled.");
     }
 }
+
