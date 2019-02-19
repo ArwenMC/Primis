f22380f1d736ed80e8981e408e536f63ce940ae7
Christopher Nethercott <ccnethercott@gmail.com>
2019-02-19 21:19:34 +0000

    Update FlyCommand.java

--- src/main/java/com/arwenmc/command/FlyCommand.java
+++ src/main/java/com/arwenmc/command/FlyCommand.java
@@ -1,5 +1,6 @@
 package com.arwenmc.command;
 
+import com.arwenmc.Primis;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -7,12 +8,16 @@ import org.bukkit.entity.Player;
 
 public class FlyCommand implements CommandExecutor {
 
+    Primis plugin;
+
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (!(sender instanceof Player)) {
             Player player = (Player) sender;
             player.sendMessage("Test Fly Command");
             // add fly logic.
             return true;
+        } else {
+            sender.sendMessage(plugin.NOT_PLAYER);
         }
         return false;
     }
