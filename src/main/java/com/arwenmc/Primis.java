e854c9ac3fa8a17a55f69adcb9b24e37307a5c79
Christopher Nethercott <ccnethercott@gmail.com>
2019-02-19 21:19:31 +0000

    Added No permission and not player messages

--- src/main/java/com/arwenmc/Primis.java
+++ src/main/java/com/arwenmc/Primis.java
@@ -1,11 +1,15 @@
 package com.arwenmc;
 
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Primis extends JavaPlugin {
 
     PluginDescriptionFile config = this.getDescription();
+
+    public final String NOT_PLAYER = ChatColor.RED + "You must be a player to use this command.";
+    public final String NO_PERMISSION = ChatColor.RED + "You do not have permission to use this command.";
 
     @Override
     public void onEnable() {
