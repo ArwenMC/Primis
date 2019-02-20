d9bbdc6a61d515388a4e310ac9684de03713acc2
Christopher Nethercott <ccnethercott@gmail.com>
2019-02-20 15:53:38 +0000

    Renamed class

--- src/main/java/com/arwenmc/listeners/PlayerEvent.java
+++ src/main/java/com/arwenmc/listeners/PlayerEvent.java
@@ -8,7 +8,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-public class OnPlayerJoinEvent implements Listener {
+public class PlayerEvent implements Listener {
 
     Primis plugin;
 
