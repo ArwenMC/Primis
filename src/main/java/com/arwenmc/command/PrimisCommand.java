0eed03416e30fbd5ce9f5087c7a9617698d1a6a2
Christopher Nethercott <ccnethercott@gmail.com>
2019-02-20 16:14:05 +0000

    Added the Primis command for config and such

---
+++ src/main/java/com/arwenmc/command/PrimisCommand.java
@@ -0,0 +1,14 @@
+package com.arwenmc.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class PrimisCommand implements CommandExecutor {
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        // add logic
+        return false;
+    }
+
+}
