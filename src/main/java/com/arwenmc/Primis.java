99b2c9cd6d973fe5aa01dba1a70bccea35ebbee6
Christopher Nethercott <ccnethercott@gmail.com>
2019-02-20 15:52:11 +0000

    Create FirstJoinEvent Listener

--- src/main/java/com/arwenmc/Primis.java
+++ src/main/java/com/arwenmc/Primis.java
@@ -1,14 +1,18 @@
 package com.arwenmc;
 
+import com.arwenmc.listeners.FirstJoinEvent;
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Primis extends JavaPlugin {
 
     PluginDescriptionFile plugin = this.getDescription();
     FileConfiguration config = this.getConfig();
+    PluginManager pm = Bukkit.getPluginManager();
 
     public final String NOT_PLAYER = (config.getString("messages.not_player") == null) ? ChatColor.RED + "You must be a player to use this command." : config.getString("messages.not_player");
     public final String NO_PERMISSION = (config.getString("messages.no_permission") == null) ? ChatColor.RED + "You do not have permission to use this comnmand." : config.getString("messages.no_permission");
@@ -16,6 +20,8 @@ public class Primis extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info(plugin.getFullName() + " version: " + plugin.getVersion() + " has been enabled.");
+
+        pm.registerEvents(new FirstJoinEvent(), this);
 
         config.options().copyDefaults(true);
         saveConfig();
---
+++ src/main/java/com/arwenmc/listeners/FirstJoinEvent.java
@@ -0,0 +1,26 @@
+package com.arwenmc.listeners;
+
+import com.arwenmc.Primis;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class FirstJoinEvent implements Listener {
+
+    Primis plugin;
+
+    @EventHandler
+    public void onPlayerJoinEvent(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        if (!player.hasPlayedBefore()) {
+            for (Player p : Bukkit.getOnlinePlayers()) {
+                String msg = ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.on_first_join").replace("{PLAYER}", player.getDisplayName()));
+                p.sendMessage(msg);
+            }
+        }
+    }
+
+}
